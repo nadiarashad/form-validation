59c811435ae0db2dd23a6310dc48d8ba1ea48632
nadiarashad <nadiarashid@live.com>
2020-03-03 17:03:32 +0000

    completed form submission validation

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ username.addEventListener("change", (event) => {
 	if (username.value.match(/\d/ && /[!@£#$%"^&*]/)) {
 		username.classList.add("valid");
 		username.classList.remove("invalid");
+
 		const usernameLable = document.querySelector("#username");
 		if (usernameLable.children.length > 1) { //use the if statement for firstname instead
 			for (let i = 1; i < usernameLable.children.length; i++) {
@@ -92,3 +93,60 @@ surname.addEventListener("keyup", (event) => {
 	}
 })
 
+
+const dob = document.querySelector("#dobInput")
+console.log(dob)
+
+const invalidMsg3 = document.createElement("p")
+
+dob.addEventListener("change", (event) => {
+
+	console.log(dob.value)
+	const year = Number(dob.value.split("-")[0])
+	const d = new Date()
+	const n = d.getFullYear()
+	console.log(year)
+	if ((n - year) >= 18) {
+		dob.classList.add("valid");
+		dob.classList.remove("invalid");
+
+		if (invalidMsg3.hidden === false) { //found with console.dir(invalidMsg)
+			invalidMsg3.hidden = true
+		}
+
+	} else {
+		dob.classList.add("invalid");
+		dob.classList.remove("valid");
+
+		invalidMsg3.innerText =
+			"You must be 18 years or older to proceed";
+
+
+		const dobLabel = document.querySelector("#dob");
+
+		if (dobLabel.children.length === 1) {
+			dobLabel.appendChild(invalidMsg3);
+		}
+
+		if (invalidMsg3.hidden === true) {
+			invalidMsg3.hidden = false
+		}
+	}
+})
+
+
+const submitbutton = document.querySelector("#button")
+
+
+submitbutton.addEventListener('submit', (event) => {
+
+	if (firstName.classList !== "valid" || username.classList !== "valid" || surname.classList !== "valid" || dob.classList !== "valid") {
+
+
+		event.preventDefault()
+
+
+	}
+
+
+})
