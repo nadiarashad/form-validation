4ac6f8d64ea356acb1d5183cc0993f873c9b5a0a
nadiarashad <nadiarashid@live.com>
2020-03-03 14:30:45 +0000

    added error msg for invalid username

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const username = document.querySelector("#usernameInput");
 console.log(username.value);
 
 username.addEventListener("change", (event) => {
-	if ((/\d/ && /[!@£#$%^&*]/).test(username.value)) {
+	if (username.match(/\d/ && /[!@£#$%"^&*]/)) {
 		username.classList.add("valid");
 		username.classList.remove("invalid");
 	} else {
 		username.classList.add("invalid");
 		username.classList.remove("valid");
+
+		// const invalidMsg = 
 	}
 });
