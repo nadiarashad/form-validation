669993b46efeecfd2e389263160181e53c9c377f
AVSheard <a_sheard@yahoo.co.uk>
2020-03-03 15:12:33 +0000

    we got a nice little error meesage to a apear and dissapear for username

--- index.js
+++ index.js
@@ -2,16 +2,26 @@
 
 const username = document.querySelector("#usernameInput");
 
-console.log(username.value);
-
 username.addEventListener("change", (event) => {
-	if (username.match(/\d/ && /[!@£#$%"^&*]/)) {
+	if (username.value.match(/\d/ && /[!@£#$%"^&*]/)) {
 		username.classList.add("valid");
 		username.classList.remove("invalid");
+		const usernameLable = document.querySelector("#username");
+		if (usernameLable.children.length > 1) {
+			for (let i = 1; i < usernameLable.children.length; i++) {
+				usernameLable.children[i].innerText = "";
+			}
+		}
 	} else {
 		username.classList.add("invalid");
 		username.classList.remove("valid");
 
-		// const invalidMsg = 
+		const invalidMsg = document.createElement("p");
+
+		invalidMsg.innerText =
+			"Invalid username, must contain at least 1 special character and 1 number";
+		const usernameLable = document.querySelector("#username");
+		console.log(usernameLable);
+		usernameLable.appendChild(invalidMsg);
 	}
 });
